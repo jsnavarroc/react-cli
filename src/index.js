96a1f495d326ab405746015ebd3db47d9260705a
Johan Navarro <21sebas12@gmail.com>
2018-02-05 20:22:28 -0500

    'Routes config whit children '

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+//Dependencies  
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { Render } from 'react-dom';
+//Routes
+import AppRoutes from './components/Private/Routes/AppRoutes' 
+
+//Assets
 import './index.css';
-import App from './components/App';
+// import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+render(
+    <Router >
+        <AppRoutes/>
+    </Router>, document.getElementById('root')
+    );
 registerServiceWorker();
