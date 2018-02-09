43b2a3a9282a973d1c628577c79023d291c4d5d9
Johan Navarro <21sebas12@gmail.com>
2018-02-09 16:54:58 -0500

    React 6 coneccion back y Inicializar Redux con Handlebars

---
+++ src/server/api/blog.js
@@ -0,0 +1,39 @@
+// Dependencies 
+import express from 'express';
+import fetch from 'node-fetch';
+
+// Data 
+import post from '../../components/Private/Date/post.json';
+import posts from '../../components/Private/Date/posts.json';
+
+// Express Router
+const Router = express.Router();
+
+
+Router.get('/post',(req, res, next) => {
+    res.json(post)
+});
+
+Router.get('/posts',(req, res, next) => {
+    res.json(posts)
+});
+
+          
+Router.get('/movies',(req, res, next) => {
+    fetch('http://localhost:3001/api/movies')
+    .then(res => res.json())
+    .then(body => res.json(body));
+});
+
+Router.get('/theater',(req, res, next) => {
+    fetch('http://localhost:3001/api/teathers')
+    .then(res => res.json())
+    .then(body => res.json(body));
+});
+
+
+
+
+
+export default Router; 
+
